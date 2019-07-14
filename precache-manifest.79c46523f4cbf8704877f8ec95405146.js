b262e3e474b78eb72e2eb25f688d1e6cce319819
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-14 21:01:42 +0000

    Deploy isqua/braining to github.com/isqua/braining.git:gh-pages

---
+++ precache-manifest.79c46523f4cbf8704877f8ec95405146.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e5fd02894f687da95df",
+    "url": "/css/app.a6684bf6.css"
+  },
+  {
+    "revision": "1e5fd02894f687da95df",
+    "url": "/js/app.dcfb099a.js"
+  },
+  {
+    "revision": "c4f0bf0d39ae411060b6",
+    "url": "/js/chunk-vendors.a4cfc558.js"
+  },
+  {
+    "revision": "397efefeb30b9a18eb99e8627c26bb2c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
